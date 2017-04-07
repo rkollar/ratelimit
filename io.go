f0c27347ab0d18d1f72483d2a5b2dd52a1d5f1d2
Roman Kollár <roman.kollar@moderntv.eu>
2017-04-07 14:22:43 +0200

    init

---
+++ io.go
@@ -0,0 +1,63 @@
+package ratelimit
+
+import (
+	"io"
+)
+
+const chunkSize = 32 * 1024
+
+type Writer struct {
+	w      io.Writer
+	bucket *Bucket
+}
+
+func NewWriter(w io.Writer, rate int64) *Writer {
+	return &Writer{
+		w:      w,
+		bucket: NewBucketWithRate(rate, capacityFromRate(rate)),
+	}
+}
+
+func (self *Writer) Write(p []byte) (n int, err error) {
+	var n_ int
+	var chunk []byte
+
+	for pos := 0; pos < len(p); pos += chunkSize {
+		chunk = p[pos:min(len(p), pos+chunkSize)]
+
+		self.bucket.Wait(int64(len(chunk)))
+
+		n_, err = self.w.Write(chunk)
+		n += n_
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (self *Writer) Close() error {
+	closer, ok := self.w.(io.Closer)
+	if ok {
+		return closer.Close()
+	}
+	return nil
+}
+
+func (self *Writer) SetLimit(rate int64) {
+	self.bucket.Set(rate, capacityFromRate(rate))
+}
+func (self *Writer) FillBucket() {
+	self.bucket.Fill()
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func capacityFromRate(rate int64) int64 {
+	return rate * 6
+}
---
+++ ratelimit.go
@@ -0,0 +1,71 @@
+package ratelimit
+
+import (
+	"time"
+)
+
+type Bucket struct {
+	ts       time.Time
+	rate     int64
+	capacity int64
+	avail    int64
+}
+
+func NewBucketWithRate(rate int64, capacity int64) *Bucket {
+	b := &Bucket{
+		ts:    time.Now().Round(1 * time.Millisecond),
+		avail: capacity,
+	}
+	b.Set(rate, capacity)
+
+	return b
+}
+
+func (self *Bucket) Set(rate int64, capacity int64) {
+	if rate <= 0 {
+		panic("rate <= 0")
+	}
+	if rate < 1000 {
+		panic("rate < 1000")
+	}
+	if capacity <= 0 {
+		panic("capacity <= 0")
+	}
+	self.rate = int64(float64(rate) / 1000)
+	self.capacity = capacity
+}
+
+func (self *Bucket) Wait(count int64) {
+	if count <= self.avail {
+		self.avail -= count
+		return
+	}
+
+	// refill the bucket
+	diff := int64(time.Since(self.ts).Nanoseconds() / 1000000)
+	self.avail += diff * self.rate
+	if self.avail > self.capacity {
+		self.avail = self.capacity
+	}
+	self.ts = self.ts.Add(time.Duration(diff) * time.Millisecond)
+
+	// re-check
+	if count <= self.avail {
+		self.avail -= count
+		return
+	}
+
+	// we need to wait
+	count -= self.avail
+	self.avail = 0
+	dur := time.Duration((float64(count) / float64(self.rate))) * time.Millisecond
+	if dur < 1*time.Millisecond {
+		dur = 1 * time.Millisecond
+	}
+	time.Sleep(dur)
+	self.Wait(count)
+}
+
+func (self *Bucket) Fill() {
+	self.avail = self.capacity
+}
