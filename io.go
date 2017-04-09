8e31b346c72d3fdccfba62026744197942dcce03
Roman Kollár <roman.kollar@moderntv.eu>
2017-04-10 00:51:57 +0200

    let the caller handle partial writes of Writer

--- io.go
+++ io.go
@@ -19,21 +19,13 @@ func NewWriter(w io.Writer, maxChunkSize int, rate int64, capacity int64) *Write
 }
 
 func (self *Writer) Write(p []byte) (n int, err error) {
-	var n_ int
-	var chunk []byte
-
-	for pos := 0; pos < len(p); pos += self.maxChunkSize {
-		chunk = p[pos:min(len(p), pos+self.maxChunkSize)]
-
-		self.bucket.Wait(int64(len(chunk)))
-
-		n_, err = self.w.Write(chunk)
-		n += n_
-		if err != nil {
-			return
-		}
+	if len(p) > self.maxChunkSize {
+		p = p[:self.maxChunkSize]
 	}
-	return
+
+	self.bucket.Wait(int64(len(p)))
+
+	return self.w.Write(p)
 }
 
 func (self *Writer) Close() error {
